f0b325e4d133a1ae5f2d9ff5d4002f6b3de57826
mehdi <mehdi.rhazi13@gmail.com>
2016-07-22 17:14:10 +0200

    personnalisation des différentes vues utilisateur

--- src/MEHDI/UserBundle/Entity/User.php
+++ src/MEHDI/UserBundle/Entity/User.php
@@ -11,10 +11,81 @@ use FOS\UserBundle\Model\User as BaseUser;
  */
 class User extends BaseUser
 {
-  /**
+	/**
+	*@var \DateTime
+	* @ORM\Column(name="dateInscription", type="datetime")
+	*
+	*/
+	private $dateInscription;
+	
+	/**
+	*@var \String
+	* @ORM\Column(name="adresse", type="string", length=255)
+	*
+	*/
+	private $adresse;
+	
+	
+/**
    * @ORM\Column(name="id", type="integer")
    * @ORM\Id
    * @ORM\GeneratedValue(strategy="AUTO")
    */
+   
   protected $id;
+
+  public function __construct(){
+	  parent::__construct();
+	  $this->dateInscription=new \DateTime();
+	  //correspond à la date système
+	  
+  }
+  
+    /**
+     * Set dateInscription
+     *
+     * @param \DateTime $dateInscription
+     *
+     * @return User
+     */
+    public function setDateInscription($dateInscription)
+    {
+        $this->dateInscription = $dateInscription;
+
+        return $this;
+    }
+
+    /**
+     * Get dateInscription
+     *
+     * @return \DateTime
+     */
+    public function getDateInscription()
+    {
+        return $this->dateInscription;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return User
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
 }
