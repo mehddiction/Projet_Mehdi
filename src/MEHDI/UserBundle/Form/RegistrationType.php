673f0fd904b163b3b4879daae9a8501e54bb4c20
mehdi <mehdi.rhazi13@gmail.com>
2016-07-26 17:42:08 +0200

    FINITION PARTIE USER & DEBUT PARTIE PRODUIT

---
+++ src/MEHDI/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MEHDI\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('adresse');
+    }
+
+    //la classe RegistrationFormType est ancêtre de celle-ci et sera donc celle utilisée
+	public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+	
+}
